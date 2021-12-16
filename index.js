1f4ff6fdb00634f0b47dcfaec2c68c574ee75ad2
NayarahSantos <nayarah.santos@hotmail.com>
2021-12-15 21:02:10 -0300

    Alterações da Segunda Aula

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function carregamentoPagina() {
-    alert("Mensagem apresentada no carregamento do sistema ");
-}
+// function carregamentoPagina() {
+//     alert("Mensagem apresentada no carregamento do sistema ");
+// }
+
+$(() => {
+    //     //código executado no carregamento da página
+    //     alert("Mensagem apresentada no carregamento do sistema ");
+
+    $("#btnSalvar").click (() => {
+        console.log("FUNÇÃO CLICK FUNCIONOU")
+    })
+})
+
